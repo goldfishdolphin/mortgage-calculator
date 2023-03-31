3d748a0a6722ac1fcd94ef68d442fd7e0b047875
Naureen <naureen.imran123@yahoo.com>
2023-03-31 16:59:18 +0100

    Make Slider

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import Navbar from './Components/Navbar';
 import Result from './Components/Result';
 import SlideSelect from './Components/SliderSelect';
 import TenureSelect from './Components/TenureSelect';
+
 function App() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanPayment: 3000 * .08,
+    loanTerm: 5,
+    interestRate: 5,
+  });
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth='xl' sx={{ marginTop: 4 }}>
         <Grid container spacing={5} alignItems="center">
           <Grid item xs={12} md={6} >
-            <SlideSelect />
+            <SlideSelect data={data} setData={setData} />
             <TenureSelect />
           </Grid>
           <Grid item xs={12} md={6}>
--- src/Components/SliderSelect.jsx
+++ src/Components/SliderSelect.jsx
@@ -1,7 +1,27 @@
-function SliderSelect() {
+import React from "react";
+import SliderComponent from "./Common/SliderComponent";
+function SliderSelect({ data, setData }) {
+    const bank_limit = 10000;
     return (
         <>
-            SliderSelect
+            <SliderComponent
+                onChange={(e, value) => {
+                    setData({
+                        ...data,
+                        homeValue: value.toFixed(0),
+                        downPayment: (0.2 * value).toFixed(0),
+                        loanAmount: (0.8 * value).toFixed(0),
+                    });
+                }}
+                defaultValue={data.homeValue}
+                min={1000}
+                max={bank_limit}
+                steps={100}
+                unit="£"
+                amount={data.homeValue}
+                label="Home Value"
+                value={data.homeValue}
+            />
         </>
     );
 }
