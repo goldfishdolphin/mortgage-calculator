954aab1d6a6a6bfd879ae846d001ada777034672
Naureen <naureen.imran123@yahoo.com>
2023-04-03 15:12:40 +0100

    Fix typos

--- src/Components/SliderSelect.jsx
+++ src/Components/SliderSelect.jsx
@@ -22,6 +22,41 @@ function SliderSelect({ data, setData }) {
                 label="Home Value"
                 value={data.homeValue}
             />
+            <SliderComponent
+                onChange={(e, value) => {
+                    setData({
+                        ...data,
+                        downPayment: value.toFixed(0),
+                        loanAmount: (data.homeValue).toFixed(0),
+                    });
+                }}
+                defaultValue={data.downPayment}
+                min={0}
+                max={data.homeValue}
+                steps={100}
+                unit="£"
+                amount={data.downPayment}
+                label="Down Payment"
+                value={data.downPayment}
+            />
+            <SliderComponent
+                onChange={(e, value) => {
+                    setData({
+                        ...data,
+                        loanAmount: value.toFixed(0),
+                        downPayment: (data.homeValue - value).toFixed(0),
+                    });
+                }}
+                defaultValue={data.loanAmount}
+                min={0}
+                max={data.homeValue}
+                steps={100}
+                unit={'£'}
+                amount={data.loanAmount}
+                label="Loan Amount"
+                value={data.loanAmount}
+            />
+
         </>
     );
 }
